a21b70320c1015a2995a4ad1c0229f5d957fec78
Erkan Çalışkan <146715229+erkan011@users.noreply.github.com>
2024-04-29 18:37:06 +0300

    Nullables, Kotlin'de null değerlerini temsil etmek için kullanılan bir kavramdır. Bu kavram, Java gibi diğer dillerde sıkça karşılaşılan NullPointerException (NPE) hatalarını engellemek için geliştirilmiştir. Nullables, bir değişkenin null olabileceğini belirtir ve bu değişkenlerle çalışırken null kontrolleri yapılmasını sağlar.
    Nullables'ın kullanımı şu şekildedir:
    
        Null Değer Atama: Null değer alabilen bir değişken tanımlamak için değişken tipinin sonuna ? işaretini eklersiniz. Örneğin, var name: String? = null şeklinde bir tanımlama yapabilirsiniz.
    
        Null Kontrolü: Nullables'ı kullanırken sıklıkla null kontrolü yapmanız gerekir. Bunun için if-else veya ?. (safe call) operatörünü kullanabilirsiniz.Nullables'ın amacı, programların daha güvenli olmasını sağlamaktır çünkü null değerler nedeniyle oluşan hataları önler ve daha düzgün bir kod yazımı sağlar.

---
+++ src/main/kotlin/Nullables.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val name: String? = null
+    if (name == null){
+        println("Name is null")
+    }else{
+        println(name.length)
+    }
+}
+
+//fun main() {
+//    val name= "erkan"
+//    println(name.length)
+//}
