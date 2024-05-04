923dee7f1a11bf0cd9d8c68d09b30560ff31326d
Erkan Çalışkan <146715229+erkan011@users.noreply.github.com>
2024-05-04 19:22:31 +0300

    ("""     int: $int     double: $double     float: $float     long: $long     short: $short     byte: $byte """.trimIndent())

---
+++ src/main/kotlin/numbers.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val int = 10
+    val double = 3.24
+    val float = 2.5f
+    val long = 242350924524
+    val short = 1203
+    val byte = 6
+
+    println("""
+    int: $int
+    double: $double
+    float: $float
+    long: $long
+    short: $short
+    byte: $byte
+""".trimIndent())
+
+}
